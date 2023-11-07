49fa1d13eb056b3ecb504cc3dbbf2942278f422a
Timothy Cai <timothyc435@gmail.com>
2023-11-07 14:35:11 -0800

    added age in 2049

---
+++ Notes/age_in_2049.py
@@ -0,0 +1,9 @@
+# Age 2049 Program
+# Author: Tim
+# 3 Nov 2023
+import time
+
+print("Hello future user I am the robot from the future and I am here to tell you your age in 2049")
+time.sleep(1)
+age = int(input("First things, first what is your age?")) + 26
+print("Congrats, here is  your date", age)
